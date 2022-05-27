a59e2d3fa77c325f549368d9a48bd3b56fbc3e18
최종수 <chlwhdtn03@naver.com>
2022-05-27 20:54:45 +0900

    주요 객체(포터블) 추가

--- app/src/main/java/com/chlwhdtn/legacy_portableserver/MainActivity.kt
+++ app/src/main/java/com/chlwhdtn/legacy_portableserver/MainActivity.kt
@@ -42,5 +42,8 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+
+
 }
 
---
+++ app/src/main/java/com/chlwhdtn/legacy_portableserver/Portable.kt
@@ -0,0 +1,18 @@
+package com.chlwhdtn.legacy_portableserver
+
+/**
+ *
+ */
+class Portable {
+
+    fun save() { // 저장 메소드
+
+    }
+
+    fun delete() { // 삭제 메소드
+
+    }
+
+
+
+}
