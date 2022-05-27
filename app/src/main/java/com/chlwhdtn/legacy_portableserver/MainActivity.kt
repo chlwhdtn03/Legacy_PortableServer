f9ab87fbed498569c42ca86edfaf43c4bbbfd6c4
최종수 <chlwhdtn03@naver.com>
2022-05-27 15:15:40 +0900

    라우터 추가

--- app/src/main/java/com/chlwhdtn/legacy_portableserver/MainActivity.kt
+++ app/src/main/java/com/chlwhdtn/legacy_portableserver/MainActivity.kt
@@ -7,25 +7,40 @@ import io.netty.util.internal.logging.InternalLoggerFactory
 import io.netty.util.internal.logging.JdkLoggerFactory
 import io.vertx.core.Vertx
 import io.vertx.core.VertxOptions
+import io.vertx.core.http.HttpClientOptions
 import io.vertx.core.http.HttpServerOptions
+import io.vertx.core.http.HttpServerResponse
+import io.vertx.ext.web.Route
+import io.vertx.ext.web.Router
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val port = 8080
+    private val vertx: Vertx = Vertx.vertx(VertxOptions().setWorkerPoolSize(40))
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         InternalLoggerFactory.setDefaultFactory(JdkLoggerFactory.INSTANCE)
 
-        val vertx = Vertx.vertx(VertxOptions().setWorkerPoolSize(40))
 
-        vertx.createHttpServer(HttpServerOptions().setPort(8080))
-            .requestHandler { req ->
-                req.response().end("YOU CONNECTED TO PHONE")
-            }
-            .listen(8080)
-            .onSuccess {
-                println("HTTP server started on port " + it.actualPort())
-            }
+        createRouter(vertx, "Earth")
+        createRouter(vertx, "Solar")
+
     }
+
+    private fun createRouter(vertx: Vertx, address: String) {
+        val route: Route = Router.router(vertx).route(address)
+
+        route.handler {
+            val response: HttpServerResponse = it.response()
+            response.putHeader("content-type", "text/plane")
+            response.end("Hello $address")
+        }
+
+    }
+
 }
+
